e7c7ed5001513aac84f42dac25854a0914394a43
tayyabakhanum <132939199+tayyabakhanum@users.noreply.github.com>
2023-11-15 14:56:26 +0400

    created a server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require ("express");
+
+const app= express();
+const PORT= 3000;
+
+app.get("/", (req,res)=>{
+
+    res.send(`<h1>HELLO WORLD</h1>`)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port ${PORT}`);
+})
+
+
